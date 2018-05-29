0e168c56486c265831ea36cdde6751bd7e60176f
usarneme <jamestcoop@gmail.com>
2018-05-28 22:52:21 -0700

    Set up boiletplate React appliacation code: webpack config, babelrc, created public and src directory structure to hold components and built app files.

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {render} from 'react-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import HeaderAppBar from './components/Header'
+import AppRouter from './routers/AppRouter'
+import './styles/styles.css'
+
+const App = () => (
+  <MuiThemeProvider>
+  <AppRouter />
+  </MuiThemeProvider>
+)
+
+render(<App />, document.getElementById('app'))
